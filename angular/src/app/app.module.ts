198a226f5f9c5fce86d92513119f19409a3452b1
Mads Barth <mads-barth@live.dk>
2023-04-13 17:49:53 +0200

    rename component folder for app to main

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './components/app/app.component';
+import { AppComponent } from './components/main/app.component';
 import { CreateUserComponent } from './components/create-user/create-user.component';
 import { AddFriendsComponent } from './components/add-friends/add-friends.component';
 
