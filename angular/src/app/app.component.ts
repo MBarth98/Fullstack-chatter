fea337b2418252cf2c5b75ae4044be613872fc92
Mads Barth <mads-barth@live.dk>
2023-04-13 14:53:11 +0200

    clean git
    rm .vscode
    
    
    Revert "rm .vscode"
    
    This reverts commit 0f1f52c5073bdff371227b4eeb56aaabaada8c6b.
    
    Auto stash before revert of "rm .vscode"
    rm firebase log

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FireService, User } from './firebase.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular';
+
+  @Input() dest_email: string = "";
+  @Input() src_email: string = "";
+
+  onAddFriend() {
+    this.fire.addFriend(this.src_email, this.dest_email);
+  }
+
+
+  @Input() name: string = "";
+  @Input() email: string = "";
+
+  onAddUser() {
+    this.fire.addUser(this.name, this.email);
+  }
+
+  constructor(private fire: FireService) {
+    fire.getUsers().then((users) => {
+      users.forEach((user) => {
+        console.log(JSON.stringify(user));
+      });
+    });
+  }
 }
